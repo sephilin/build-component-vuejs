03144ae0e49e3e87c2899acd3a94d34d9b36f931
sephilin <vitor.drj@gmail.com>
2020-06-24 18:05:39 -0400

    initial commit

---
+++ src/time-vue.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+  Time now:
+  <br/>
+  <span>{{ time }}</span>
+  </div>
+</template>
+<script>
+import moment from 'moment';
+
+const timeVue = {
+  data() {
+    return {    
+      time: ''
+    };
+  },
+  mounted: function(){
+      const  self = this;
+      setInterval(() => {
+          const dateNow = new Date(Date.now());
+          self.time = moment(dateNow).format('MMMM Do YYYY, h:mm:ss a');
+      }, 1000);
+  }
+};
+
+export default timeVue;
+</script>
